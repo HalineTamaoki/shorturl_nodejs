9aaf0d3b6173609b36606c60c7ed44e1d31743fd
Haline Tamaoki <104457218+HalineTamaoki@users.noreply.github.com>
2022-07-18 22:04:38 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
     API_URL: 'http://localhost:3000',
-    MONGO_CONNECTION: 'mongodb+srv://HalineTamaoki:aAJ1fH9y3SWv3Hcw@encurtadorurl.bgvdmap.mongodb.net/encurtadorURL?retryWrites=true&w=majority'
+    MONGO_CONNECTION: 'mongodb+srv://<username>:<password>@encurtadorurl.bgvdmap.mongodb.net/encurtadorURL?retryWrites=true&w=majority'
 }
